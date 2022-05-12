2980fbcfaa6de80433efa24c001956e6b84ccbb5
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-12 18:11:58 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -18,6 +18,7 @@ public class FirstTest {
         options.addArguments("disable-gpu");
         options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
         options.addArguments("--privileged");
+        options.setExperimentalOption("useAutomationExtension", true);
         driver = new ChromeDriver(options);
         driver.get("https://www.impactguru.com/");
         System.out.println("Title of the page is: " + driver.getTitle());
