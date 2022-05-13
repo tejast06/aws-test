9705dbd1c5386787bf951714da4563ba369f9be4
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-13 21:19:33 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,30 +12,18 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
        
-        //old one
-       // WebDriver driver;
-       //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        //ChromeOptions options = new ChromeOptions();
-       // options.addArguments("browser");
-       // options.addArguments("disable-gpu");
-       // options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-      //  options.addArguments("--privileged");
-       // driver = new ChromeDriver(options);
-    //    driver.get("https://www.impactguru.com/");
-     //   System.out.println("Title of the page is: " + driver.getTitle());
-    
-        //New one
+        
+        WebDriver driver;
        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-       ChromeOptions options = new ChromeOptions();
-       options.addArguments("start-maximized"); // open Browser in maximized mode
-       options.addArguments("disable-infobars"); // disabling infobars
-       options.addArguments("--disable-extensions"); // disabling extensions
-       options.addArguments("--disable-gpu"); // applicable to windows os only
-       options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-       options.addArguments("--no-sandbox"); // Bypass OS security model
-       WebDriver driver = new ChromeDriver(options);
-       driver.get("https://www.impactguru.com");
-        System.out.println("Title of the page is: " + driver.getTitle()); 
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("browser");
+        options.addArguments("disable-gpu");
+        options.addArguments("--privileged");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.impactguru.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+   
     }
 
 
