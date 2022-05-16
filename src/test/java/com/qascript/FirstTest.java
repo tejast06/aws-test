b871cd3a3eefed5340016dd7f93d8e7a0b8134e6
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-16 17:02:42 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,28 +5,25 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class FirstTest {
 
 
     @Test
-	 WebDriver driver;  
-	    System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-	    options.addArguments("--privileged");
-	   options.addArguments("--no-sandbox");
-	options.addArguments("window-size=1400,800");    
-        options.addArguments("browser");
-       options.addArguments("disable-gpu");
-	//options.addArguments("--disable-features=VizDisplayCompositor");
-        driver = new ChromeDriver(options);
-	driver.manage().window().maximize();
-        driver.get("https://www.impactguru.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Crowdfunding in India: Best Fundraising Platform Online | ImpactGuru"));
- 
+	System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+	ChromeOptions options = new ChromeOptions();
+	 WebDriver driver = new ChromeDriver(options);
+	 options.setHeadless(true);
+	 driver.get("https://www.impactguru.com");
+	 driver.quit();
 	
-   
+	    
     }
 
 
