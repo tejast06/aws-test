a852c53d79c258bbf5d4968e6100a305f8a5a4ff
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-16 14:51:27 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -14,16 +14,22 @@ public class FirstTest {
        
     WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
+        //ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("browser");
        //options.addArguments("disable-gpu");
      // options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
       //options.addArguments("--privileged");
+       // driver = new ChromeDriver(options);
+      // driver.get("https://www.impactguru.com");
+      // System.out.println("Title of the page is: " + driver.getTitle());
+        //System.out.println("Hello Team");
+        
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+// Instantiate the chrome driver
         driver = new ChromeDriver(options);
-       driver.get("https://www.impactguru.com");
-       System.out.println("Title of the page is: " + driver.getTitle());
-        System.out.println("Hello Team");
+        driver.get("https://www.impactguru.com");
    
         
  
