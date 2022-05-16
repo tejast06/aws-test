04675f4664ef2a29f258d558b6ffc6f94dddcefa
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-16 16:43:44 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -10,22 +10,13 @@ public class FirstTest {
 
 
     @Test
-    public void OpenBrowser()  {
-   WebDriver driver;  
-	    System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-	    options.addArguments("--privileged");
-	   options.addArguments("--no-sandbox");
-	options.addArguments("window-size=1400,800");    
-        options.addArguments("browser");
-       options.addArguments("disable-gpu");
-	//options.addArguments("--disable-features=VizDisplayCompositor");
-        driver = new ChromeDriver(options);
-	driver.manage().window().maximize();
-        driver.get("https://www.impactguru.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Crowdfunding in India: Best Fundraising Platform Online | ImpactGuru"));
- 
+	 ChromeOptions options = new ChromeOptions();
+	 options.setHeadless(true);
+         System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+	 WebDriver driver = new ChromeDriver(options);
+	 driver.get("https://www.impactguru.com");
+	
+   
     }
 
 
