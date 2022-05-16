159f682a8b4773b4b480892057ee91b7bc799054
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-16 15:19:41 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -14,17 +14,9 @@ public class FirstTest {
        
     WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");			
-		options.addArguments("headless");
+	    options.addArguments("headless");
         options.addArguments("browser");		
-		options.addArguments("start-maximized"); 
-        options.addArguments("enable-automation"); 
-        options.addArguments("--no-sandbox"); 
-        options.addArguments("--disable-infobars"); 
-        options.addArguments("--disable-dev-shm-usage"); 
-        options.addArguments("--disable-browser-side-navigation"); 
-        options.addArguments("--disable-gpu"); 
-        options.addArguments("--disable-features=VizDisplayCompositor"); 
-		ChromeOptions options = new ChromeOptions();
+	ChromeOptions options = new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NONE);
         driver = new ChromeDriver(options);
         driver.get("https://www.impactguru.com");
