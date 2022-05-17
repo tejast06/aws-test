a95a824fbb7c9a612ebbd0ec31f950561e8dd018
Tejas <46339378+tejast06@users.noreply.github.com>
2022-05-17 13:06:19 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class FirstTest {
 	 @Test
          public void OpenBrowser() {
-       System.setProperty("webdriver.gecko.driver", "C:\\Users\\impac\\Downloads\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+       System.setProperty("webdriver.gecko.driver", " /usr/bin/geckodriver");
 
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.impactguru.com/");
