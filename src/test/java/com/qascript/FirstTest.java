c89913ec70b13f646f0673f9042e8270bdbe76be
Tejas <46339378+tejast06@users.noreply.github.com>
2022-06-05 14:32:53 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,18 +1,18 @@
 package com.qascript;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class FirstTest {
 	 @Test
          public void OpenBrowser() {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver","F:\\Chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
         ChromeOptions options = new ChromeOptions();
          options.addArguments("headless");
         driver = new ChromeDriver(options);
