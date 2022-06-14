6a4aa5886e2f192e9f8e32e0b6c383fab6965a49
Tejas <46339378+tejast06@users.noreply.github.com>
2022-06-14 14:50:20 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class FirstTest  {
